9061e9b46fa2fd6b34ecd9d9f87381661e591831
Максим <defoltmaks@gmail.com>
2019-04-24 11:03:51 +0300

    Creation of new figures and logic of falling
    1) Created the rest of the figures
    2) The logic of the fall of the figure
    3) Check for the possibility of falling

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ for (let y = 18; y > 0; y--){
     }
 }
 
-let x = 5, y = 10; // установили первоначальные координаты для фигур (временно)
+let x = 5, y = 15; // установили первоначальные координаты для фигур (временно)
 let mainArr = [
     //палка
     [
@@ -31,6 +31,26 @@ let mainArr = [
     //квадрат
     [
         [1,0],[0,1],[1,1]
+    ],
+    //Буква L
+    [
+        [1,0],[0,1],[0,2]
+    ],
+    //Буква L (зеркальная)
+    [
+        [1,0],[1,1],[1,2]
+    ],
+    //Молния (право)
+    [
+        [1,0],[-1,1],[0,1]
+    ],
+    //Молния (лево)
+    [
+        [1,0],[1,1],[2,1]
+    ],
+    //Лего
+    [
+        [1,0],[2,0],[1,1]
     ]
 ]
 
@@ -53,5 +73,46 @@ function create(){//создание фигуры
         figureBody[i].classList.add('figure'); //зарисовка нужных клеточек
     }
 }
-
 create();
+
+function move() {
+    let moveflag = true;
+    let coordinates = [ // создание массива для фигуры
+        [figureBody[0].getAttribute('posX'), figureBody[0].getAttribute('posY')],
+        [figureBody[1].getAttribute('posX'), figureBody[1].getAttribute('posY')],
+        [figureBody[2].getAttribute('posX'), figureBody[2].getAttribute('posY')],
+        [figureBody[3].getAttribute('posX'), figureBody[3].getAttribute('posY')]
+    ];
+    for(let i = 0; i < coordinates.length; i++){// проверка на то, что мы можем двигаться вниз
+        if(coordinates[i][1] == 1 || document.querySelector(`[posX = "${coordinates[i][0]}"][posY = "${coordinates[i][1]-1}"]`).classList.contains('set')){
+            moveflag = false;
+            break;
+        }
+    }
+
+    if(moveflag){// если можем двигаться дальше
+        for (let i=0; i<figureBody.length; i++){
+            figureBody[i].classList.remove('figure'); //убираем предыдущую зарисовку
+        }
+        figureBody = [// заполнение массива со смещёнными координатами
+            document.querySelector(`[posX = "${coordinates[0][0]}"][posY = "${coordinates[0][1]-1}"]`),
+            document.querySelector(`[posX = "${coordinates[1][0]}"][posY = "${coordinates[1][1]-1}"]`),
+            document.querySelector(`[posX = "${coordinates[2][0]}"][posY = "${coordinates[2][1]-1}"]`),
+            document.querySelector(`[posX = "${coordinates[3][0]}"][posY = "${coordinates[3][1]-1}"]`)
+        ];
+        for (let i=0; i < figureBody.length; i++){
+            figureBody[i].classList.add('figure'); //зарисовка нужных клеточек (смещённых на -1)
+        }
+    }
+    else{// если не можем двигаться дальше
+        for(let i = 0; i < figureBody.length; i++){
+            figureBody[i].classList.remove('figure');
+            figureBody[i].classList.add('set');
+        }
+        create();// создаём новую фигуру
+    }
+}
+
+let interval = setInterval(() => {
+    move();
+}, 300);
